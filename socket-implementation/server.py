1a6c92c920d99a686fcc3d455477848d7c28d9c5
Ryan Sakurai <ryansakurai03@gmail.com>
2023-01-16 00:11:40 -0300

    refactor server

--- socket-implementation/server.py
+++ socket-implementation/server.py
@@ -2,7 +2,6 @@ import os
 import threading
 import socket
 
-
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 1024
 BUFFER_SIZE = 1024*4
@@ -10,14 +9,10 @@ FILE_FOLDER_PATH = "files/"
 
 
 def receive_file(client_socket: socket.socket, file_path: str) -> None:
-    if not os.path.exists(file_path):
-        client_socket.send(True.to_bytes(1))
-    else:
-        client_socket.send(False.to_bytes(1))
+    if os.path.exists(file_path):
         raise FileExistsError()
-
-    if not os.path.isdir("files"):
-        os.makedirs("files")
+    if not os.path.isdir(FILE_FOLDER_PATH):
+        os.makedirs(FILE_FOLDER_PATH)
 
     with open(file_path, "wb") as file:
         while True:
@@ -29,10 +24,7 @@ def receive_file(client_socket: socket.socket, file_path: str) -> None:
 
 
 def send_file(client_socket: socket.socket, file_path: str) -> None:
-    if os.path.exists(file_path):
-        client_socket.send(True.to_bytes(1))
-    else:
-        client_socket.send(False.to_bytes(1))
+    if not os.path.exists(file_path):
         raise FileNotFoundError()
 
     with open(file_path, mode="rb") as file:
@@ -52,33 +44,45 @@ def decode_data_received(data: bytes) -> tuple[str, str]:
     - name of the file the client wants to send or receive
     """
 
-    data = data[2:].decode()
-    return data[0].capitalize(), data[1:]
+    data = data[2:].decode() # handles Java modified UTF encoding
+    command, path_received =  data[0].capitalize(), data[1:]
+    file_name = os.path.basename(path_received)
+    return command, file_name
+
+
+def is_possible_to_receive(file_path_in_server: str) -> bool:
+    return not os.path.exists(file_path_in_server)
+
+
+def is_possible_to_send(file_path_in_server: str) -> bool:
+    return os.path.exists(file_path_in_server)
 
 
 def handle_client(client_socket: socket.socket, client_adress: tuple):
     print(f"Connection started with {client_adress[0]}:{client_adress[1]}")
     
     command, file_name = decode_data_received(client_socket.recv(BUFFER_SIZE))
-    file_name = os.path.basename(file_name)
-    file_path = FILE_FOLDER_PATH + file_name
+    file_path_in_server = FILE_FOLDER_PATH + file_name
 
     if command == "S":
-        try:
-            print(f"Command: send {file_name}")
-            receive_file(client_socket, file_path)
-        except FileExistsError:
-            print(f"{file_name} not received, there already is a {file_name}")
-        else:
+        print(f"Command: send {file_name}")
+        if is_possible_to_receive(file_path_in_server):
+            client_socket.send(True.to_bytes(1))
+            receive_file(client_socket, file_path_in_server)
             print(f"{file_name} received")
-    elif command == "R":
-        try:
-            print(f"Command: receive {file_name}")
-            send_file(client_socket, file_path)
-        except FileNotFoundError:
-            print(f"{file_name} not sent, there is not a {file_name}")
         else:
+            client_socket.send(False.to_bytes(1))
+            print(f"{file_name} not received, there already is a {file_name}")
+
+    elif command == "R":
+        print(f"Command: receive {file_name}")
+        if is_possible_to_send(file_path_in_server):
+            client_socket.send(True.to_bytes(1))
+            send_file(client_socket, file_path_in_server)
             print(f"{file_name} sent")
+        else:
+            client_socket.send(False.to_bytes(1))
+            print(f"{file_name} not sent, there is not a {file_name}")
 
     client_socket.close()
     print(f"Connection ended with {client_adress[0]}:{client_adress[1]}")
